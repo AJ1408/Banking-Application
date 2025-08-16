f31b268b660b181070a12956603812433f398e11
Ashish <ashishraj0609@gmail.com>
2025-08-17 02:22:38 +0530

    Created Addcustomer page

--- Frontend/Banking Management system/src/Components/AddCustomer.jsx
+++ Frontend/Banking Management system/src/Components/AddCustomer.jsx
@@ -48,7 +48,7 @@ export default function AddCustomer() {
           onChange={handleChange}
           required
         />
-        <button type="submit">Add</button>
+        <button type="submit">Add Details </button>
       </form>
     </div>
   );
